cdafbd1f4acefb72c0a4423d432c54233196a3ae
Jason Lipowicz <jason.lipowicz@gmail.com>
2020-06-07 22:56:02 +0100

    Persisting connection

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -25,31 +25,44 @@ class App extends Component<{}, AppState> {
     name: null,
     game_id: null,
     entered_game: false,
-    user_id: uuidv4()
+    user_id: null
   }
 
   reconnect() {
-    console.log('connecting...')
-    const ws = new WebSocket(URL)
-    ws.onopen = () => {
-      console.log('connected')
-    }
+    return new Promise((res, rej) => {
+      console.log('connecting...')
+      const ws = new WebSocket(URL)
+      ws.onopen = () => {
+        console.log('connected')
+        res()
+      }
 
-    ws.onmessage = evt => {
-      const message = JSON.parse(evt.data)
-      console.log(message)
-      this.setState({ ...message, entered_game: true })
-    }
+      ws.onmessage = evt => {
+        const message = JSON.parse(evt.data)
+        console.log(message)
+        this.setState({ ...message, entered_game: true })
+      }
 
-    ws.onclose = () => {
-      console.log('disconnected')
-      setTimeout(this.reconnect.bind(this), 1000)
-    }
-    this.ws = ws
+      ws.onclose = () => {
+        console.log('disconnected')
+        setTimeout(this.reconnect.bind(this), 1000)
+      }
+      this.ws = ws
+    })
   }
 
-  componentDidMount() {
-    this.reconnect()
+  async componentDidMount() {
+    await this.reconnect()
+    const user_id = localStorage.getItem('user_id')
+    const name = localStorage.getItem('name')
+    const game_id = localStorage.getItem('game_id')
+    if (user_id && game_id) {
+      this.setState({ game_id, user_id, name })
+      this.sendMessage({ type: "retrieve_game", game_id, user_id })
+    }
+    else {
+      this.setState({ user_id: uuidv4() })
+    }
   }
 
   sendMessage(data) {
@@ -58,15 +71,30 @@ class App extends Component<{}, AppState> {
 
   join() {
     const { name, game_id, user_id } = this.state
+    localStorage.setItem('user_id', user_id)
+    localStorage.setItem('game_id', game_id)
+    localStorage.setItem('name', name)
     this.sendMessage({ type: "new_player", value: name, game_id, user_id })
+  }
+
+  logout() {
+    localStorage.removeItem('user_id')
+    localStorage.removeItem('game_id')
+    localStorage.removeItem('name')
+    location.reload()
   }
 
   render() {
     return (
       <div className="App">
-        Your Name: <input type="text" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} /><br />
-        Join a game: <input type="text" value={this.state.game_id} onChange={(e) => this.setState({ game_id: e.target.value })} /><br />
-        <button onClick={() => this.join()}>Join</button>
+        {
+          !this.state.entered_game &&
+          <>
+            Your Name: <input type="text" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} /><br />
+            Join a game: <input type="text" value={this.state.game_id} onChange={(e) => this.setState({ game_id: e.target.value })} /><br />
+            <button onClick={() => this.join()}>Join</button>
+          </>
+        }
         <hr />
         {
           this.state.entered_game &&
@@ -74,6 +102,7 @@ class App extends Component<{}, AppState> {
             <b>Game ID: {this.state.game_id}</b><br />
             Players:<br />
             { this.state.players.map(p => <div>{p.name}</div>) }
+            <br /><a href="#" onClick={() => this.logout()}>Logout</a>
           </div>
         }
       </div>
--- src/start.ts
+++ src/start.ts
@@ -14,15 +14,17 @@ const wss = new Server({ port: process.env.NODE_ENV === 'development' ? 3001 : 8
 wss.on('connection', (ws: WebSocket) => {
   ws.on('message', (data: string) => {
 
+
     let message: Message = JSON.parse(data)
     let adapter = new FileSync(message.game_id + '.json')
     let db = low(adapter)
 
     db.defaults({ players: [] }).write()
 
+    clients[message.user_id] = ws
+
     switch (message.type) {
       case "new_player":
-        clients[message.user_id] = ws
         db.get('players').push({
           user_id: message.user_id,
           name: message.value
