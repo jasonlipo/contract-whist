77cbc5c40d21d6444ee5b3315b36aa3a7cf5675d
Jason Lipowicz <jason.lipowicz@gmail.com>
2020-06-15 21:40:35 +0100

    Try/catch to BE. Closed #10

--- src/start.ts
+++ src/start.ts
@@ -19,29 +19,34 @@ const wss = new WebSocketServer({ httpServer: server });
 wss.on('request', ws => {
   const connection = ws.accept(null, ws.origin);
   connection.on('message', raw => {
-    let controller_action: boolean = true;
-    let message: IMessage = JSON.parse(raw.utf8Data)
-    const filename = 'data/' + message.game_id + '.json'
-    controller_action = VerifyGame(connection, message, filename)
-    if (!controller_action) return;
+    try {
+      let controller_action: boolean = true;
+      let message: IMessage = JSON.parse(raw.utf8Data)
+      const filename = 'data/' + message.game_id + '.json'
+      controller_action = VerifyGame(connection, message, filename)
+      if (!controller_action) return;
 
-    let adapter = new FileSync(filename)
-    let db = low(adapter)
+      let adapter = new FileSync(filename)
+      let db = low(adapter)
 
-    initialise(db, message)
-    clients[message.user_id] = connection
+      initialise(db, message)
+      clients[message.user_id] = connection
 
-    switch (message.type) {
-      case "create_player": case "join_player": controller_action = CreateJoinPlayer(db, connection, message); break;
-      case "start_game": controller_action = StartGame(db, message, deck); break;
-      case "submit_prediction": controller_action = SubmitPrediction(db, message, deck); break;
-      case "submit_trump": controller_action = SubmitTrump(db, message); break;
-      case "play_card": controller_action = PlayCard(db, message); break;
-      case "next_trick": controller_action = NextTrick(db); break;
-      case "get_scores": controller_action = GetScores(db); break;
-      case "next_round": controller_action = NextRound(db, message, deck);break;
+      switch (message.type) {
+        case "create_player": case "join_player": controller_action = CreateJoinPlayer(db, connection, message); break;
+        case "start_game": controller_action = StartGame(db, message, deck); break;
+        case "submit_prediction": controller_action = SubmitPrediction(db, message, deck); break;
+        case "submit_trump": controller_action = SubmitTrump(db, message); break;
+        case "play_card": controller_action = PlayCard(db, message); break;
+        case "next_trick": controller_action = NextTrick(db); break;
+        case "get_scores": controller_action = GetScores(db); break;
+        case "next_round": controller_action = NextRound(db, message, deck);break;
+      }
+
+      BroadcastResponse(db, clients, controller_action, filename, message)
     }
-
-    BroadcastResponse(db, clients, controller_action, filename, message)
+    catch (e) {
+      console.error('Error: ' + e)
+    }
   });
 });
