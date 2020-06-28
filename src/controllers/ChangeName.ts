8687267a2a1cd53577f5eb9dec30f1967f2ea271
Jason Lipowicz <jason.lipowicz@gmail.com>
2020-06-28 11:43:24 +0100

    If changing name fails, still return response

--- src/controllers/ChangeName.ts
+++ src/controllers/ChangeName.ts
@@ -2,18 +2,16 @@ import { fetch_players, log, IMessage } from '../utils';
 
 export const ChangeName = async (db: any, message: IMessage): Promise<boolean> => {
   const all_players = fetch_players(db)
-  if (all_players.indexOf(message.value) > -1) {
-    // Name already exists
-    return false;
+  if (all_players.indexOf(message.value) == -1) {
+    await db.set(['private', message.user_id, 'name'], message.value,).write()
+    const new_players = all_players.map((p, i) => {
+      if (i == message.player_index) {
+        return message.value
+      }
+      return p
+    })
+    await db.set('shared.players', new_players).write()
+    await log(db, message, `changed their name to ${message.value}`)
   }
-  await db.set(['private', message.user_id, 'name'], message.value,).write()
-  const new_players = all_players.map((p, i) => {
-    if (i == message.player_index) {
-      return message.value
-    }
-    return p
-  })
-  await db.set('shared.players', new_players).write()
-  await log(db, message, `changed their name to ${message.value}`)
   return true;
 }
