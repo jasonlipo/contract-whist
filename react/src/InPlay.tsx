01d5d564a446c492109e16cf90bb4a0ddf82804a
Jason Lipowicz <jason.lipowicz@gmail.com>
2020-06-30 21:03:04 +0100

    Another horrible fix

--- react/src/InPlay.tsx
+++ react/src/InPlay.tsx
@@ -7,15 +7,19 @@ interface IInPlayProps extends IContractWhistState {
 }
 
 export const InPlay: FC<IInPlayProps> = ({ in_play, player_index, timer_seconds, hand, send, table, player_lead_trick }) => {
-  const can_select = canSelectCard(table, player_lead_trick, player_index, hand)
-  let automatic_play_card = 0
-  while (!can_select(hand[automatic_play_card])) {
-    automatic_play_card++
-  }
+  let can_select
+  if (in_play == player_index) {
+    can_select = canSelectCard(table, player_lead_trick, player_index, hand)
+    let automatic_play_card = 0
+    while (!can_select(hand[automatic_play_card])) {
+      automatic_play_card++
+    }
 
-  return in_play == player_index && timer_seconds !== null && (
-    <div className="actions">
-      <CountdownTimer seconds={timer_seconds} onComplete={() => send({ type: "play_card", value: automatic_play_card, card_id: hand[automatic_play_card] })} />
-    </div>
-  )
+    return in_play == player_index && timer_seconds !== null && (
+      <div className="actions">
+        <CountdownTimer seconds={timer_seconds} onComplete={() => send({ type: "play_card", value: automatic_play_card, card_id: hand[automatic_play_card] })} />
+      </div>
+    )
+  }
+  return null
 }
