082d8a71a436e82061b9f31177e5ebd8525f297c
Jason Lipowicz <jason.lipowicz@gmail.com>
2020-07-07 21:18:20 +0100

    Randomise autoplay card. Closed #42

--- react/src/InPlay.tsx
+++ react/src/InPlay.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { IContractWhistState } from './ContractWhist';
 import { CountdownTimer } from './CountdownTimer';
 import { canSelectCard } from './HandOfCards';
+import _ from 'lodash';
 
 interface IInPlayProps extends IContractWhistState {
 }
@@ -11,13 +12,17 @@ export const InPlay: FC<IInPlayProps> = ({ in_play, player_index, timer_seconds,
   if (in_play == player_index) {
     can_select = canSelectCard(table, player_lead_trick, player_index, hand)
     let automatic_play_card = 0
-    while (!can_select(hand[automatic_play_card])) {
+    let randomise_hand = _.shuffle(hand.map((_x, i) => i))
+    while (!can_select(hand[randomise_hand[automatic_play_card]])) {
       automatic_play_card++
     }
 
     return in_play == player_index && timer_seconds !== null && (
       <div className="actions">
-        <CountdownTimer seconds={timer_seconds} onComplete={() => send({ type: "play_card", value: automatic_play_card, card_id: hand[automatic_play_card] })} />
+        <CountdownTimer
+          seconds={timer_seconds}
+          onComplete={() => send({ type: "play_card", value: randomise_hand[automatic_play_card], card_id: hand[randomise_hand[automatic_play_card]] })}
+        />
       </div>
     )
   }
