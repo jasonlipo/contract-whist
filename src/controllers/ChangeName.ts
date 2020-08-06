1af2440792f4a5854c75dd8742c118ba5a69ef07
Jason Lipowicz <jason.lipowicz@gmail.com>
2020-08-06 22:27:48 +0100

    Compare name conflicts case insensitive

--- src/controllers/ChangeName.ts
+++ src/controllers/ChangeName.ts
@@ -2,7 +2,7 @@ import { fetch_players, log, IMessage, ELogAction } from '../utils';
 
 export const ChangeName = async (db: any, message: IMessage): Promise<boolean> => {
   const all_players = fetch_players(db)
-  if (all_players.indexOf(message.value) == -1) {
+  if (all_players.map(x => x.toLowerCase()).indexOf(message.value.toLowerCase()) == -1) {
     await db.set(['private', message.user_id, 'name'], message.value).write()
     const new_players = all_players.map((p, i) => {
       if (i == message.player_index) {
